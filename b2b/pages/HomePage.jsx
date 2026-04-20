f7e4b41ac33710fd1a546afdc4db90b8271b61d4
connect668 <connect@initiativeenterprises.net>
2026-04-20 15:26:18 -0500

    Add ContactSection import to HomePage

--- b2b/pages/HomePage.jsx
+++ b2b/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import ServicesSection from "../components/ServicesSection";
 import PilotSection from "../components/PilotSection";
+import ContactSection from "../components/ContactSection";
 
 export default function HomePage() {
   return (
