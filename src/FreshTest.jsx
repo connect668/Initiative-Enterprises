aed7bbfcb26da4bcbf4d4d0d7c0afebcc0f87c13
connect668 <connect@initiativeenterprises.net>
2026-04-20 21:15:10 -0500

    Add FreshTest component with styled div

---
+++ src/FreshTest.jsx
@@ -0,0 +1,17 @@
+export default function FreshTest() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "red",
+        minHeight: "100vh",
+        fontSize: "40px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      FRESH TEST
+    </div>
+  );
+}
