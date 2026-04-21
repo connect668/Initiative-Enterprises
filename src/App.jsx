2a4926650551141f79f9d443fd5c22f32025c6c7
connect668 <connect@initiativeenterprises.net>
2026-04-20 21:06:25 -0500

    Refactor App component to return a heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
 export default function App() {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#050505",
-        color: "white",
-        display: "grid",
-        placeItems: "center",
-        fontFamily: "Arial, sans-serif",
-      }}
-    >
-      <h1>test</h1>
-    </main>
-  );
+  return <h1 style={{ color: "red" }}>APP TEST</h1>;
 }
