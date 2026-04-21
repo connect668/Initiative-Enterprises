7aed5150a8d7b5287cbd4399468fea7c42d4f27c
connect668 <connect@initiativeenterprises.net>
2026-04-20 21:04:07 -0500

    Replace App component with TestApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+
+function TestApp() {
+  return <h1 style={{ color: "red" }}>REACT TEST</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TestApp />
   </React.StrictMode>
 );
-
