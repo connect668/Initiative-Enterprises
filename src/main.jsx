eff674cdf5d3ba1844386aa19bc5ff8de8691118
connect668 <connect@initiativeenterprises.net>
2026-04-20 21:16:52 -0500

    Implement FreshTest component in main.jsx
    Added a FreshTest component that displays a styled message.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FreshTest from "./FreshTest.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FreshTest />
-  </React.StrictMode>
-);
+function FreshTest() {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "lime",
+        minHeight: "100vh",
+        fontSize: "40px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      INLINE FRESH TEST
+    </div>
+  );
+}
+
+try {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <FreshTest />
+    </React.StrictMode>
+  );
+} catch (e) {
+  document.body.innerHTML = `<pre style="color:red;background:black;min-height:100vh;padding:24px;">${String(e)}</pre>`;
+}
