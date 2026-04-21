377071b5cbedebec759a2e1083a3f193caf4fe22
connect668 <connect@initiativeenterprises.net>
2026-04-21 00:11:08 -0500

    Add landing fallback shell

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,27 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+const fallbackMarkup = rootElement.innerHTML;
+
+function restoreFallback() {
+  if (!rootElement.innerHTML.trim()) {
+    rootElement.innerHTML = fallbackMarkup;
+  }
+}
+
+window.addEventListener("error", restoreFallback);
+window.addEventListener("unhandledrejection", restoreFallback);
+
+try {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
+} catch (error) {
+  restoreFallback();
+  throw error;
+}
